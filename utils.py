9669875a30fbb7eb9e09fb39f07211a9b23db992
parthagg <parthagg@umich.edu>
2020-03-20 18:18:00 -0400

    readme, setup

---
+++ utils.py
@@ -0,0 +1,21 @@
+
+# using python data classes 
+@dataclass
+class pose:
+    x: float = 0.0
+    y: float = 0.0
+    vx: float = 0.0 
+    vy: float = 0.0
+
+
+
+
+
+
+# old defintion of pose, not using @dataclass 
+# class pose:
+#     def __init__(self, x=0, y=0, vx=0, vy=0): 
+#         self.x = x
+#         self.y = y 
+#         self.vx = vx 
+#         self.vy = vy 
