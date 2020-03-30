d58a682addbe30a60e444c351c72d1bb8fffce04
parthagg <parthagg@umich.edu>
2020-03-30 17:54:16 -0400

    corner detection working

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 #from utils import *
 from video_reader import *
 import cv2
-
+import time # remove this
 
 
 
@@ -19,10 +19,21 @@ if __name__ == "__main__":
     
     print("Warning: No GUI")
     reader = Reader(0, True)
+
+    _frames = 0 
+    _last_update_time = time.time()
+
     while True:
         img = reader.get_frame()
-        cv2.imshow("img", img)
-        cv2.waitKey(1)
+        
+        # cv2.imshow("img", img)
+        # cv2.waitKey(1)
+        _frames += 1
+        if _frames > 59:
+            print("frame rate: {} fps".format(_frames / (time.time() - _last_update_time)))
+            _frames = 0
+            _last_update_time = time.time()
+
 #    reader = Reader(0, False)
 #    while True:
 #        img = reader.get_frame()
--- mapping.py
+++ mapping.py
@@ -10,52 +10,94 @@
 #       - detect_walls(): detect walls in the image
 #     - **Forwards results to:** planning.py 
 #     -  **Output:** path 
-import constants
+from constants import *
 import utils
 import numpy as np
 import cv2
-# class Map:
-#     def __init__(self, image):
-#         image_corners = detect_corners(image)
+import matplotlib.pyplot as plt
+class Map:
+    def __init__(self, image):
+        image_corners = detect_corners(image)
 
-#         warped_corners = np.array(
-#             [
-#                 [0 ,0],
-#                 [HEIGHT_IN_CELLS, 0],
-#                 [HEIGHT_IN_CELLS, WIDTH_IN_CELLS],
-#                 [0, WIDTH_IN_CELLS]
-#             ]
-#         )
-#         homography = fit_homography(image_corners, map_corners)
-#         warped_image = apply_homography(image, homography)
-#         self.map = np.zeros((HEIGHT_IN_CELLS, WIDTH_IN_CELLS))
-#         self.detect_walls(warped_image)
-#         self.detect_holes(warped_image)
+        warped_corners = np.array(
+            [
+                [0 ,HEIGHT_IN_CELLS], # bottom-left
+                [WIDTH_IN_CELLS, HEIGHT_IN_CELLS], # bottom-right
+                [0, 0], #top-left
+                [WIDTH_IN_CELLS, 0] # top-right
+            ],
+            dtype="float32")
+
+        transform = cv2.getPerspectiveTransform(image_corners, warped_corners)
+        warped = cv2.warpPerspective(image, transform, image.shape[0:2])
+        cv2.imshow("Warped", warped)
+        cv2.waitKey(0)
+
+        # self.map = np.zeros((HEIGHT_IN_CELLS, WIDTH_IN_CELLS))
+        # self.detect_walls(warped_image)
+        # self.detect_holes(warped_image)
         
    
 
 
 
-#     def fit_homography(image_corners, map_corners):
-#         pass
+    def fit_homography(image_corners, map_corners):
+        pass
     
-#     def apply_homography(image, homography):
-#         pass
+    def apply_homography(image, homography):
+        pass
 
+
+    
 def detect_corners(img):
-    hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h,s,v = cv2.split(hsv_image)
 
-    th, threshed = cv2.threshold(s, 100, 255, cv2.THRESH_OTSU|cv2.THRESH_BINARY)
-    print("saving result")
-    cv2.imwrite("result.png", threshed)
-    print("saved")
+    # create NumPy arrays from the boundaries
+    lower = np.array([100, 60, 150], dtype = "uint8") # 131, 49, 72
+    upper = np.array([170, 100, 200], dtype = "uint8") # 169, 87, 145
+
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(img, lower, upper)
+    mask = cv2.blur(mask, (5,5))
     
+    # cv2.imshow("Mask", mask)
+    # cv2.waitKey(0)
 
+
+    output = cv2.bitwise_and(img, img, mask = mask)
+   
+    im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:4]
+
+    print(len(contours))
+
+    corners = np.zeros((4 ,2), dtype="float32")
+    i = 0
+    for c in contours:
+        M = cv2.moments(c)
+        if M["m00"] == 0: 
+            continue
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        # cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
+        # cv2.putText(img, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        # cv2.imshow("Image", img)
+        # cv2.waitKey(0)
+        corners[i][0] = cX
+        corners[i][1] = cY
+        i += 1
+    print("i: ",i)
+    # cv2.imshow("Image", img)
+    # cv2.waitKey(0)
+    return corners # bottom-left, bottom-right, top-left, top-right
+
+    
 def main():
-    print("in main")
     img = cv2.imread("test_image.png")
-    detect_corners(img)
+    # detect_corners(img)
+    map = Map(img)
 
 if __name__ == "__main__":
     main()
