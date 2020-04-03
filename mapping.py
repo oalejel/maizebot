9fb90799a3aaa63d9e4c33bec69702aca2968877
parthagg <parthagg@umich.edu>
2020-04-03 19:19:13 -0400

    mapping almost working

--- mapping.py
+++ mapping.py
@@ -30,7 +30,6 @@ class Map:
                 [precrop_width, precrop_height], # bottom-right
                 [precrop_width, 0], # top-right
                 [0, 0], #top-left
-
             ],
             dtype="float32")
 
@@ -49,7 +48,7 @@ class Map:
 
         self.map = np.zeros((HEIGHT_IN_CELLS, WIDTH_IN_CELLS))
         self.detect_holes(cropped)
-        # self.detect_walls(np.copy(cropped))
+        self.detect_walls(np.copy(cropped))
         
    
 
@@ -61,13 +60,13 @@ class Map:
 
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         lower = np.array([155, 115, 165], dtype = "uint8") # 131, 49, 72
-        upper = np.array([170, 180, 220], dtype = "uint8")
+        upper = np.array([175, 180, 220], dtype = "uint8")
 
         mask = cv2.inRange(hsv, lower, upper)
         mask = cv2.blur(mask, (5,5))
 
-        # cv2.imshow("Mask", mask)
-        # cv2.waitKey(0)
+        cv2.imshow("Mask", mask)
+        cv2.waitKey(0)
 
         im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         contours = sorted(contours, key = cv2.contourArea, reverse = True)[:4]
@@ -92,85 +91,158 @@ class Map:
             corners[i][1] = cY
             i += 1
         print("i: ",i)
-        # cv2.imshow("Image", img)
-        # cv2.waitKey(0)
+        cv2.imshow("Image", img)
+        cv2.waitKey(0)
         return corners # bottom-left, bottom-right, top-left, top-right
 
 
+    # def detect_walls(self, img):
+    #     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+    #     img = cv2.blur(img, (5,5))
+
+        
+    #     threshold = 40
+        
+    #     gray[gray>threshold] = 255
+    #     gray[gray<threshold] = 0
+
+    #     cv2.imshow("GRAY", gray)
+    #     cv2.waitKey(0)
+
+    #     self.map[gray==0] = 1
+
     def detect_walls(self, img):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # cv2.imshow("h", hsv[:,:,0])
+        # cv2.waitKey(0)
+
+        # cv2.imshow("s", hsv[:,:,1])
+        # cv2.waitKey(0)
+
+        # cv2.imshow("v", hsv[:,:,2])
+        # cv2.waitKey(0)
+        
+
+
+        # cv2.waitKey(0)
         # cv2.imshow("GRAY", img)
         # cv2.waitKey(0)
 
-        img = cv2.blur(img, (5,5))
+        
+        img[self.map == 2] = 255
+        # img = cv2.blur(img, (5,5))
+
+        cv2.imshow("gray", img)
+        cv2.waitKey(0)
+
+        bw_threshold = 60
+        
+        img[img> bw_threshold] = 255
+        img[img< bw_threshold] = 0
+        img = 255 - img
+        cv2.imshow("Filtered", img) 
+        cv2.waitKey(0)
+
+        ero0 = 2
+        kernel = np.ones((ero0, ero0), np.uint8) 
+        img = cv2.erode(img, kernel)
+        cv2.imshow("erosion 0", img)
+        cv2.waitKey(0)
+
+        dil1 = 10  
+        kernel = np.ones((dil1, dil1), np.uint8) 
+        img = cv2.dilate(img, kernel)
+        cv2.imshow("Dilation1", img)
+        cv2.waitKey(0)
+
+        ero1 = 16
+        kernel = np.ones((ero1, ero1), np.uint8) 
+        img = cv2.erode(img, kernel)
+        cv2.imshow("erosion 1", img)
+        cv2.waitKey(0)
+
+
+        dil2 = 9
+        kernel = np.ones((dil2, dil2), np.uint8) 
+        img = cv2.dilate(img, kernel)
+        cv2.imshow("dilation 2", img)
+        cv2.waitKey(0)
 
         low_threshold = 0
         high_threshold = 10
-        
-        img[img>20] = 255
-        # cv2.imshow("Blurred", img)
+
+        # edges = cv2.Canny(img, low_threshold, high_threshold)
+
+        # cv2.imshow("Edges", edges)
         # cv2.waitKey(0)
 
-        edges = cv2.Canny(img, low_threshold, high_threshold)
 
-        cv2.imshow("Edges", edges)
-        cv2.waitKey(0)
+        # rho = 1  # distance resolution in pixels of the Hough grid
+        # theta = np.pi / 180  # angular resolution in radians of the Hough grid
+        # threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+        # min_line_length = 50  # minimum number of pixels making up a line
+        # max_line_gap = 20  # maximum gap in pixels between connectable line segments
+        # line_image = np.copy(img) * 0  # creating a blank to draw lines on
 
+        # # Run Hough on edge detected image
+        # # Output "lines" is an array containing endpoints of detected line segments
+        # lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+        #                     min_line_length, max_line_gap)
 
-        rho = 1  # distance resolution in pixels of the Hough grid
-        theta = np.pi / 180  # angular resolution in radians of the Hough grid
-        threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-        min_line_length = 50  # minimum number of pixels making up a line
-        max_line_gap = 20  # maximum gap in pixels between connectable line segments
-        line_image = np.copy(img) * 0  # creating a blank to draw lines on
+        # for line in lines:
+        #     for x1,y1,x2,y2 in line:
+        #         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+        # # Draw the lines on the  image
+        # # lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
 
-        # Run Hough on edge detected image
-        # Output "lines" is an array containing endpoints of detected line segments
-        lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-                            min_line_length, max_line_gap)
+        # cv2.imshow("Detected walls", line_image)
+        # cv2.waitKey(0)
 
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
-        # Draw the lines on the  image
-        lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
-
-        cv2.imshow("Blurred", lines_edges)
-        cv2.waitKey(0)
 
     def detect_holes(self, img):
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        lower = np.array([97, 204, 115], dtype = "uint8") # 131, 49, 72
-
-
-        h = 203, 204, 198, 202, 214.3, 199.4, 202.5
-
-        s = 89, 88, 100, 83, 96, 100, 88
-
-        v = 48, 47, 64, 47, 63, 78, 68, 46
-
-        # h = 195, 215
-        # s = 80, 100
-        # v = 45, 80
-
+        lower = np.array([97, 204, 115], dtype = "uint8")
         upper = np.array([108, 255, 204], dtype = "uint8")
 
         mask = cv2.inRange(hsv, lower, upper)
+        # mask = cv2.blur(mask, (5,5))
 
-        cv2.imshow("Mask", mask)
-        cv2.waitKey(0)
-
-        self.map[mask] = 2
-
-        print("i: ",i)
-        # cv2.imshow("Image", img)
+        # cv2.imshow("Mask", mask)
         # cv2.waitKey(0)
-        return holes # bottom-left, bottom-right, top-left, top-right
+
+
+        dil1 = 12
+        kernel = np.ones((dil1, dil1), np.uint8) 
+        mask = cv2.dilate(mask, kernel)
+        # cv2.imshow("Dilation1", mask)
+        # cv2.waitKey(0)
+
+
+        # ero1 = 1
+        # kernel = np.ones((ero1, ero1), np.uint8) 
+        # mask = cv2.erode(mask, kernel)
+        # cv2.imshow("erode1", mask)
+        # cv2.waitKey(0)
+
+
+        # cv2.imshow("Mask", mask)
+        # cv2.waitKey(0 )
+
+        threshold = 10
+
+        self.map[mask > 10] = 2
+        # cv2.imshow("map", self.map)
+        # cv2.waitKey(0)
+
 
     
 def main():
-    img = cv2.imread("sample_frames/image1.png")
+    img = cv2.imread("sample_frames/image2.png")
     # detect_corners(img)
     map = Map(img)
 
