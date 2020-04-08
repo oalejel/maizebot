a38217500d910d0e3cf8706c5d36e6105c26f9e7
oalejel <alejel.omar@gmail.com>
2020-04-08 16:53:18 -0400

    gui array -> image -> rendering complete

--- gui.py
+++ gui.py
@@ -3,6 +3,9 @@ from tkinter import *
 import time
 import numpy as np
 from PIL import Image, ImageTk
+from numpy import asarray
+import math
+import threading
 
 # Able to visualize map, updated whenever updat emap is called
 # Can show and hide trace with key command 
@@ -10,7 +13,6 @@ from PIL import Image, ImageTk
 # public interface variables:
 	# show_trace, 
 # public interface methods:
-	# 
 
 class MazeGUI(Frame): 
 	
@@ -29,26 +31,28 @@ class MazeGUI(Frame):
 		
 		self.show_trace = False
 		self.trace = list() # pairs of (x, y, time)
-		self.maze = list() # 2d map of maze
-		self.maze_w = 300
-#		self.maze_h = 200
-		self.canvas_h = 300
-		self.canvas_w = 500
+		
 		self.maze_array = maze_array # save np.ndarray representing maze
+		
+		# compute dimensions of canvas to fit maze, potentially scaling
+		MINIMUM_HEIGHT = 300
+		PANEL_WIDTH = 200
+		PANEL_PADDING = 20
+		self.scale_factor = math.ceil(MINIMUM_HEIGHT / maze_array.shape[0])
+		self.canvas_h = self.scale_factor * self.maze_array.shape[0]
+		self.canvas_w = self.scale_factor * self.maze_array.shape[1] + PANEL_WIDTH
 		
 		self.initUI()
 		
-#		self.make_fake_map()
-		self.colorize_map() # converts to red white and black
-		self.draw_map()
-		print("done drawing map")
+		self.generate_map_image() # converts to red white and black
+		self.redraw_map_image()
+		
 		# warning: may need to put this on a separate thread
 		self.master.mainloop()
 
 	def initUI(self):
 #		self.root.geometry("400x250")
 		self.master.title("MaizeBot GUI")
-#		self.masterpack(side = TOP, pady = 5)
 
 		canvas = Canvas(self.master, width=self.canvas_w, height=self.canvas_h)#, bg="blue")
 #		canvas.create_rectangle(0, 0, 100, 100, fill="red")
@@ -67,72 +71,91 @@ class MazeGUI(Frame):
 	
 	def updateBall(x, y): 
 		self.trace.append(x, y, time.time())
+		self.circle = self.canvas.create_circle(20, x, y, fill="#BBB", outline="")
+		print(self.circle)
 #		self.canvas.create_
 		
 	# convert map of 0,1,2 to colors 
-	def colorize_map(self): 
+	def generate_map_image(self): 
+		# 0 empty 1 wall, 2 hole
 		color_arr = np.ndarray(shape=(self.maze_array.shape[0], self.maze_array.shape[1], 3))
-		color_arr[:,:,0] = (self.maze_array[:, :] == 1) * 255 # red if 1
-		color_arr[:,:,0] = (self.maze_array[:, :] == 2) * 125 # gray if 2
-		color_arr[:,:,1] = (self.maze_array[:, :] == 2) * 125 # gray if 2
-		color_arr[:,:,2] = (self.maze_array[:, :] == 2) * 125 # gray if 2
-		
+		color_arr[self.maze_array[:, :] == 1,0] = 255 # red if 1
+		color_arr[self.maze_array[:, :] == 2,0:3] = 125 # gray if 2
+				
 		# save our maze array to color 
 		self.maze_array = color_arr
-#		print(self.maze_array.shape)
-#		quit()
-#		self.maze_array[x==1] = 
-#		0 empty 1 wall, 2 hole
 		
-	# called when we want to refresh the map????
-	def draw_map(self):
-		Image.
-		maze_img = ImageTk.PhotoImage(image=Image.fromarray(self.maze_array), mode="RGB")
+		img = Image.fromarray(np.uint8(self.maze_array))
+		maze_img = ImageTk.PhotoImage(image=img.resize((self.scale_factor * self.maze_array.shape[1], self.scale_factor * self.maze_array.shape[0])))
 		self.maze_img = maze_img # avoid garbage collection
-		self.canvas.create_image(0, 0, image=maze_img, anchor=NW)
-#		print("here")
-#		self.maze_scale = min(self.maze_w / float(len(self.maze[0])), self.canvas_h / float(len(self.maze)))
-#		x = 0 
-#		y = 0
-#		for r_index, row in enumerate(self.maze): 
-#			x = 0
-#			for c_index, entry in enumerate(row):
-#				if entry == 0: # if floor, draw white at this pixel
-#					self.canvas.create_rectangle(x, y, self.maze_scale, self.maze_scale, fill="white")
-#				elif entry == 1: # wall 
-#					self.canvas.create_rectangle(x, y, self.maze_scale, self.maze_scale, fill="red")
-#				else: # hole, fill with gray 
-#					self.canvas.create_rectangle(x, y, self.maze_scale, self.maze_scale, fill="gray")
-#				x += self.maze_scale
-#				
-#			y += self.maze_scale 
-#		print("here2")
 
-		
-		
-
-	# MOCK DATA 
-	
-#	def make_fake_map(self): 
-#		array = np.zeros((100,50))
-#		array[0:40] = 125
-#		maze_img = ImageTk.PhotoImage(image=Image.fromarray(array))
-#		self.maze_img = maze_img # avoid garbage collection
-#		self.canvas.create_image(0, 0, image=maze_img, anchor=NW)
-		
-#		self.maze = list()
-#		for y in range(0, self.canvas_h):
-#			self.maze.append([0] * self.maze_w) # add a new row 
-#			for x in range(0, self.maze_w):
-#				if y < 10 or y > 290 or x < 10 or x > 390:
-#					self.maze[y][x] = 1
-#				else: 
-#					self.maze[y][x] = 0
 			
+	def redraw_map_image(self): 
+		self.canvas.create_image(0, 0, image=self.maze_img, anchor=NW)
 
-# test map
-array = np.zeros((100,50))
-array[0:40] = 125
+	
 
-# comment out
-ex = MazeGUI(array)
+#def main(): 
+#	# test map
+#	array = np.ones((500,700))
+#	array[20:480, 20:680] = 0
+#		
+#	# comment out
+#	ex = MazeGUI(array)
+#
+#main()
+
+
+
+
+
+"""
+Multithreading testing:
+	
+
+
+class Test: 
+	def __init__(self): 
+		self.foo = 50
+		
+	def print_foo(self):
+		print(self.foo)
+		self.foo = self.foo + 1
+
+def run_maizebot(): 
+	x = 0
+	while x != 1001: 
+		x = x + 2 
+		print(x)
+
+	
+
+def main(): 
+	t = Test()
+	# test map
+	array = np.ones((500,700))
+	array[20:480, 20:680] = 0
+	
+	t.print_foo()
+	print("first")
+	def do_stuff(): 
+		time.sleep(4)
+		t.print_foo()
+		print("second")
+		print("--- IM PRINTING {} ---".format(array.shape))
+	
+	thread = threading.Thread(target=do_stuff, args=())
+#	thread = threading.Thread(target=run_maizebot, args=())
+	thread.start()   
+	
+	t.print_foo()
+	print("third")
+	
+	# comment out
+	ex = MazeGUI(array, t)
+
+main()
+
+
+
+"""
