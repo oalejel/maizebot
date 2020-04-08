5a163532479e647bd818129085bd8b3d0102f4ce
oalejel <alejel.omar@gmail.com>
2020-04-08 17:56:34 -0400

    ready for ball positioning

--- gui.py
+++ gui.py
@@ -11,14 +11,13 @@ import threading
 # Can show and hide trace with key command 
 
 # public interface variables:
-	# show_trace, 
 # public interface methods:
 
 class MazeGUI(Frame): 
 	
 	def key(self, event):
 		if (event.char == 't'):
-			self.show_trace = not self.show_trace
+#			self.show_trace = not self.show_trace
 			print("toggle show_trace")
 
 	def callback(self, event):
@@ -29,8 +28,8 @@ class MazeGUI(Frame):
 	def __init__(self, maze_array):
 		super().__init__()
 		
-		self.show_trace = False
-		self.trace = list() # pairs of (x, y, time)
+#		self.show_trace = False
+#		self.trace = list() # pairs of (x, y, time)
 		
 		self.maze_array = maze_array # save np.ndarray representing maze
 		
@@ -40,40 +39,46 @@ class MazeGUI(Frame):
 		PANEL_PADDING = 20
 		self.scale_factor = math.ceil(MINIMUM_HEIGHT / maze_array.shape[0])
 		self.canvas_h = self.scale_factor * self.maze_array.shape[0]
-		self.canvas_w = self.scale_factor * self.maze_array.shape[1] + PANEL_WIDTH
+		self.canvas_w = self.scale_factor * self.maze_array.shape[1]# + PANEL_WIDTH
+		self.labels = {}
 		
-		self.initUI()
+		self.initUI() # prepare canvas
 		
-		self.generate_map_image() # converts to red white and black
-		self.redraw_map_image()
-		
+		# bind keys for toggles
+		self.canvas.bind("<Key>", self.key)
+#		self.canvas.bind("<Button-1>", self.) # for mouse clicks
+
+		# add new helpers to canvas object 
+		def _create_circle(self, x, y, r, **kwargs):
+				return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+		Canvas.create_circle = _create_circle
+
+	def run(self):
 		# warning: may need to put this on a separate thread
 		self.master.mainloop()
 
 	def initUI(self):
-#		self.root.geometry("400x250")
 		self.master.title("MaizeBot GUI")
 
 		canvas = Canvas(self.master, width=self.canvas_w, height=self.canvas_h)#, bg="blue")
-#		canvas.create_rectangle(0, 0, 100, 100, fill="red")
-		canvas.pack()
-#		canvas.create_line(15, 25, 200, 25)
-#		canvas.create_line(300, 35, 300, 200, dash=(4, 2))
-#		canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
-		
-		canvas.bind("<Key>", self.key)
-#		canvas.bind("<Button-1>", self.) # for mouse clicks
-		canvas.pack(fill=BOTH, expand=2)
+		canvas.pack(side="left")
 		self.canvas = canvas 
 		
-	def setMap(map_data):
-		pass 
-	
-	def updateBall(x, y): 
-		self.trace.append(x, y, time.time())
-		self.circle = self.canvas.create_circle(20, x, y, fill="#BBB", outline="")
-		print(self.circle)
-#		self.canvas.create_
+		self.pinball = None
+		
+		# prepare first version of UI
+		self.generate_map_image() # converts to red white and black
+		self.redraw_map_image()
+
+			
+	def update_ball(self, x, y): 
+#		self.trace.append((x, y, time.time()))
+		if self.pinball is None:
+			self.pinball = self.canvas.create_circle(x, y, 20, fill="#BBB", outline="")
+		else: 
+			self.canvas.move(self.pinball, x - self.ball_pos[0], y - self.ball_pos[1])
+		self.ball_pos = (x, y)
+
 		
 	# convert map of 0,1,2 to colors 
 	def generate_map_image(self): 
@@ -93,17 +98,58 @@ class MazeGUI(Frame):
 	def redraw_map_image(self): 
 		self.canvas.create_image(0, 0, image=self.maze_img, anchor=NW)
 
-	
+	def update_label(self, name, text):
+#		if len(text) < 30:
+#			text += ("_") * (30 - len(text))
+		if name not in self.labels: 
+			w = Label(self.master, text=text, width=20)
+			w.pack(anchor=NE, side="right", padx=5) 
 
-#def main(): 
-#	# test map
-#	array = np.ones((500,700))
-#	array[20:480, 20:680] = 0
-#		
-#	# comment out
-#	ex = MazeGUI(array)
-#
-#main()
+			self.labels[name] = w
+		else:
+			self.labels[name].configure(text=text) 
+		print(len(text))
+
+	
+# comment the testing code out 
+def main(): 
+	
+	# test map
+	array = np.ones((500,700))
+	array[20:480, 20:680] = 0
+	
+	gui = MazeGUI(array)
+	
+	def simulate_maizebot(): 
+		
+#		time.sleep(4) # testing something out 
+#		print(gui) # confirm that we have acces to the gui 
+		x = 40
+		animate_right = True
+		while 1:
+			gui.update_label("m1theta", "Motor 1 theta: {}".format(x))
+			gui.update_label("m2theta", "Motor 2 theta: {}".format(x**2))
+			time.sleep(1 / 60)
+			if animate_right: 
+				x += 2
+				if x >= 200:
+					animate_right = False
+			else:
+				x -= 2
+				if x < 40:
+					animate_right = True
+			gui.update_ball(x, 50)
+	
+	thread = threading.Thread(target=simulate_maizebot, args=())
+	thread.start()
+		
+	# run the gui on main
+	gui.run()
+	
+	
+	thread.join()
+
+main()
 
 
 
